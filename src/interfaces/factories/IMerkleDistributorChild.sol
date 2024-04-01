68046bf8e542bd972dcd549e92d21499aed0b122
Luke Hamilton <pies.eth@pm.me>
2024-03-31 22:09:03 -0400

    feat: move merkleDistributor from utils to tokens

--- src/interfaces/factories/IMerkleDistributorChild.sol
+++ src/interfaces/factories/IMerkleDistributorChild.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.20;
 
-import {IMerkleDistributor} from '@interfaces/utils/IMerkleDistributor.sol';
+import {IMerkleDistributor} from "@interfaces/tokens/IMerkleDistributor.sol";
 
-import {IFactoryChild} from '@interfaces/factories/IFactoryChild.sol';
+import {IFactoryChild} from "@interfaces/factories/IFactoryChild.sol";
 
 interface IMerkleDistributorChild is IMerkleDistributor, IFactoryChild {}
