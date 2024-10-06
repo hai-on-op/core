593344cd18c2a67ee91734c08707f907dd74f4cf
Weißer Hase <wei3erHase@protonmail.com>
2024-10-06 16:33:20 -0300

    feat: adding ExpensesAuctioneer contract

---
+++ src/contracts/governance/ExpensesAuctioneer.sol
@@ -0,0 +1,238 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.20;
+
+/*
+  Coded for Let's get HAI and the Money God with 🥕 by
+                 .__________                 ___ ___
+  __  _  __ ____ |__\_____  \  ___________  /   |   \_____    ______ ____
+  \ \/ \/ // __ \|  | _(__  <_/ __ \_  __ \/    ~    \__  \  /  ___// __ \
+   \     /\  ___/|  |/       \  ___/|  | \/\    Y    // __ \_\___ \\  ___/
+    \/\_/  \___  >__/______  /\___  >__|    \___|_  /(____  /____  >\___  >
+               \/          \/     \/              \/      \/     \/     \/
+*/
+
+import {ISAFEEngine} from '@interfaces/ISAFEEngine.sol';
+import {IOracleRelayer} from '@interfaces/IOracleRelayer.sol';
+import {IBaseOracle} from '@interfaces/oracles/IBaseOracle.sol';
+import {IModifiable} from '@interfaces/utils/IModifiable.sol';
+import {ICollateralJoin} from '@interfaces/utils/ICollateralJoin.sol';
+
+import {CollateralAuctionHouse, ICollateralAuctionHouse} from '@contracts/CollateralAuctionHouse.sol';
+import {Authorizable} from '@contracts/utils/Authorizable.sol';
+import {Disableable} from '@contracts/utils/Disableable.sol';
+import {Modifiable} from '@contracts/utils/Modifiable.sol';
+
+import {Assertions} from '@libraries/Assertions.sol';
+import {Encoding} from '@libraries/Encoding.sol';
+import {Math, RAY, WAD} from '@libraries/Math.sol';
+
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
+contract ExpensesAuctioneer is Authorizable, Modifiable, Disableable {
+  using Math for uint256;
+  using Encoding for bytes;
+  using Assertions for uint256;
+  using Assertions for address;
+
+  bytes32 public constant C_TYPE = bytes32('KITE');
+
+  ISAFEEngine public immutable safeEngine;
+
+  ICollateralJoin public protocolJoin;
+  IOracleRelayer public oracleRelayer;
+  ICollateralAuctionHouse public collateralAuctionHouse;
+
+  mapping(uint256 _auctionId => uint256 _expectedRaise) public expectedRaise;
+
+  struct ExpensesAuctioneerParams {
+    address recipient;
+    uint256 /* WAD */ usdAmountPerAuction;
+    uint256 /* WAD */ expensesPerYear;
+    uint256 /* WAD */ sellBuffer;
+    uint256 /* seconds */ cooldownPeriod;
+    uint256 /* seconds */ auctionDuration;
+  }
+
+  ExpensesAuctioneerParams public _params;
+
+  function params() external view returns (ExpensesAuctioneerParams memory) {
+    return _params;
+  }
+
+  uint256 public constant ONE_YEAR = 365 days;
+
+  // --- Init ---
+
+  constructor(
+    address _safeEngine,
+    address _protocolJoin,
+    address _oracleRelayer,
+    ExpensesAuctioneerParams memory __params
+  ) Authorizable(msg.sender) validParams updateExpenses {
+    safeEngine = ISAFEEngine(_safeEngine);
+    _setProtocolJoin(_protocolJoin);
+    oracleRelayer = IOracleRelayer(_oracleRelayer);
+    _params = __params;
+    // NOTE: not initializing collateralAuctionHouse as it needs to be deployed after this contract
+  }
+
+  error AuctionIsStillActive();
+  error AuctioneerIsInCooldown();
+  error NotEnoughDebt();
+  error NotEnoughKite();
+  error InvalidCAH();
+
+  event RaisedUSDAmount(uint256 amount);
+  event CancelledRaisedUSDAmount(uint256 amount);
+
+  uint256 internal _lastRedemptionPrice;
+  uint256 internal _accumulatedExpenses;
+  uint256 internal _processedExpenses;
+  uint256 internal _lastUpdate;
+  uint256 internal _lastAuction;
+
+  function accumulatedExpenses() external view returns (uint256) {
+    return _accumulatedExpenses;
+  }
+
+  function processedExpenses() external view returns (uint256) {
+    return _processedExpenses;
+  }
+
+  function outstandingDebt() public view returns (uint256) {
+    if (_processedExpenses >= _accumulatedExpenses) return 0;
+    return _accumulatedExpenses - _processedExpenses;
+  }
+
+  function pokeExpenses() external updateExpenses {}
+
+  modifier updateExpenses() {
+    // accumulated debt + time since last update * debt rate
+    uint256 _timestamp = block.timestamp;
+    _accumulatedExpenses += (_params.expensesPerYear * (_timestamp - _lastUpdate) / ONE_YEAR);
+    _lastUpdate = _timestamp;
+    _;
+  }
+
+  function extraordinaryExpense(uint256 _amount) external isAuthorized updateExpenses {
+    _accumulatedExpenses += _amount;
+  }
+
+  function cancelExpense(uint256 _amount) external isAuthorized updateExpenses {
+    _accumulatedExpenses -= _amount;
+  }
+
+  function joinKiteBalance() public {
+    IERC20 protocolToken = ICollateralJoin(protocolJoin).collateral();
+    uint256 _balance = protocolToken.balanceOf(address(this));
+    if (_balance == 0) return;
+    ICollateralJoin(protocolJoin).join(address(this), _balance);
+  }
+
+  function exitKiteBalance(uint256 _amount, address _to) external isAuthorized {
+    ICollateralJoin(protocolJoin).exit(_to, _amount);
+  }
+
+  /// @notice Starts an auction of KITE for HAI, with a USD amount
+  /// @dev    Checks that time has passed, and that theres enough outstanding debt
+  function auctionExpenses() external updateExpenses returns (uint256 _auctionId) {
+    // ensure balance is higher than `USD amount to auction`
+    if (outstandingDebt() < _params.usdAmountPerAuction) revert NotEnoughDebt();
+    // ensure time has passed since last auction
+    if (block.timestamp - _lastAuction < _params.cooldownPeriod) revert AuctioneerIsInCooldown();
+    // query OracleRelayer for KITE/USD price
+    uint256 _kiteUsdPrice = oracleRelayer.cParams(C_TYPE).oracle.read();
+    // query SAFEEngine for redemption price / OracleRelayer for HAI/USD price
+    uint256 _haiUsdPrice = oracleRelayer.redemptionPrice();
+    // calculate how much HAI is needed (mkt/redemption price) to cover debt
+    uint256 _amountToRaise = (_params.usdAmountPerAuction * 1e9).rdiv(_haiUsdPrice);
+    // calculate how mich KITE is needed + 10% buffer
+    uint256 _amountToSell = _params.usdAmountPerAuction.wdiv(_kiteUsdPrice);
+    // ensure KITE is available
+    joinKiteBalance();
+    if (safeEngine.tokenCollateral(C_TYPE, address(this)) < _amountToSell) revert NotEnoughKite();
+    // start auction
+    _auctionId = collateralAuctionHouse.startAuction({
+      _forgoneCollateralReceiver: address(this),
+      _auctionIncomeRecipient: _params.recipient,
+      _amountToRaise: _amountToRaise * WAD, // convert RAY to RAD
+      _collateralToSell: _amountToSell.wmul(_params.sellBuffer)
+    });
+    // register last auction timestamp
+    _lastAuction = block.timestamp;
+  }
+
+  function terminateAuction(uint256 _auctionId) external updateExpenses {
+    // check auction state
+    ICollateralAuctionHouse.Auction memory _auction = collateralAuctionHouse.auctions(_auctionId);
+    // check time has passed since auction started
+    if (block.timestamp < _auction.initialTimestamp + _params.auctionDuration) revert AuctionIsStillActive();
+    // call terminateAuctionPrematurely
+    // NOTE: it calls removeCoinsFromAuction(_auction.amountToRaise) so we need to cancel it
+    collateralAuctionHouse.terminateAuctionPrematurely(_auctionId);
+    // calculate how much USD that was trying to cover (_lastRedemptionPrice is updated in removeCoinsFromAuction)
+    uint256 _remainingToRaiseUSDAmount = (_auction.amountToRaise / WAD).rmul(_lastRedemptionPrice) / 1e9;
+    // substract the amount to cancel the effect from removeCoinsFromAuction
+    _processedExpenses -= _remainingToRaiseUSDAmount;
+    emit CancelledRaisedUSDAmount(_remainingToRaiseUSDAmount);
+  }
+
+  /// @notice This function get's called by the CAH when a bid is placed
+  /// @dev    May also be called by an authorized address to register OTC arrangements
+  function removeCoinsFromAuction(uint256 _coinAmount) external isAuthorized updateExpenses {
+    // query SAFEEngine for redemption price
+    _lastRedemptionPrice = oracleRelayer.redemptionPrice();
+    // calculate how much USD that covers
+    uint256 _raisedUSDAmount = (_coinAmount / WAD).rmul(_lastRedemptionPrice) / 1e9;
+    // substract the amount of USD from the outstanding debt
+    _processedExpenses += _raisedUSDAmount;
+    emit RaisedUSDAmount(_raisedUSDAmount);
+  }
+
+  function _modifyParameters(bytes32 _param, bytes memory _data) internal override {
+    address _address = _data.toAddress();
+
+    if (_param == 'protocolJoin') protocolJoin = ICollateralJoin(_address);
+    else if (_param == 'oracleRelayer') oracleRelayer = IOracleRelayer(_address);
+    else if (_param == 'collateralAuctionHouse') _setCollateralAuctionHouse(_address);
+    else if (_param == 'recipient') _params.recipient = _address;
+    else if (_param == 'usdAmountPerAuction') _params.usdAmountPerAuction = _data.toUint256();
+    else if (_param == 'expensesPerYear') _params.expensesPerYear = _data.toUint256();
+    else if (_param == 'sellBuffer') _params.sellBuffer = _data.toUint256();
+    else if (_param == 'cooldownPeriod') _params.cooldownPeriod = _data.toUint256();
+    else if (_param == 'auctionDuration') _params.auctionDuration = _data.toUint256();
+    else revert UnrecognizedParam();
+  }
+
+  function _setCollateralAuctionHouse(address _address) internal {
+    _address.assertHasCode();
+
+    if (address(collateralAuctionHouse) != address(0)) {
+      _removeAuthorization(address(collateralAuctionHouse));
+      safeEngine.denySAFEModification(address(collateralAuctionHouse));
+    }
+
+    collateralAuctionHouse = ICollateralAuctionHouse(_address);
+    if (collateralAuctionHouse.collateralType() != C_TYPE) revert InvalidCAH();
+    _addAuthorization(_address);
+    safeEngine.approveSAFEModification(_address);
+  }
+
+  function _setProtocolJoin(address _address) internal {
+    _address.assertHasCode();
+    IERC20 protocolToken;
+    if (address(protocolJoin) != address(0)) {
+      protocolToken = protocolJoin.collateral();
+      protocolToken.approve(address(protocolJoin), 0);
+    }
+
+    protocolToken = ICollateralJoin(_address).collateral();
+    protocolToken.approve(_address, type(uint256).max);
+    protocolJoin = ICollateralJoin(_address);
+  }
+
+  function _validateParameters() internal view override {
+    address(oracleRelayer).assertHasCode();
+    _params.recipient.assertNonNull();
+  }
+}
---
+++ test/single/ExpensesAuctioneer.t.sol
@@ -0,0 +1,295 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.20;
+
+import 'ds-test/test.sol';
+import {ICollateralAuctionHouse, CollateralAuctionHouse} from '@contracts/CollateralAuctionHouse.sol';
+import {ExpensesAuctioneer} from '@contracts/governance/ExpensesAuctioneer.sol';
+import {ISAFEEngine, SAFEEngine} from '@contracts/SAFEEngine.sol';
+import {IAccountingEngine, AccountingEngine} from '@contracts/AccountingEngine.sol';
+import {CoinJoin} from '@contracts/utils/CoinJoin.sol';
+import {CollateralJoin} from '@contracts/utils/CollateralJoin.sol';
+
+import {CoinForTest} from '@test/mocks/CoinForTest.sol';
+import {DisableableForTest} from '@test/mocks/DisableableForTest.sol';
+import {IOracleRelayer, OracleRelayerForTest} from '@test/mocks/OracleRelayerForTest.sol';
+import {OracleForTest, IBaseOracle} from '@test/mocks/OracleForTest.sol';
+
+abstract contract Hevm {
+  function warp(uint256) public virtual;
+  function prank(address) public virtual;
+  function startPrank(address) public virtual;
+  function etch(address, bytes calldata) public virtual;
+  function expectRevert(bytes4) public virtual;
+}
+
+contract SingleExpensesAuctioneerTest is DSTest {
+  Hevm hevm;
+
+  ExpensesAuctioneer expensesAuctioneer;
+  CollateralAuctionHouse kiteCollateralAuctionHouse;
+  SAFEEngine safeEngine;
+  OracleRelayerForTest oracleRelayer;
+  OracleForTest systemCoinOracle;
+  CoinForTest protocolToken;
+  OracleForTest protocolTokenOracle;
+  CollateralJoin protocolTokenJoin;
+
+  address constant RECIPIENT = address(420);
+  address constant BIDDER = address(69);
+
+  uint256 constant ONE = 10 ** 27;
+  uint256 constant MINUS_50_PERCENT_IN_10_HOURS = 999_980_746_097_009_882_063_724_393;
+  bytes32 constant KITE = bytes32('KITE');
+  uint256 constant KITE_PRICE = 10e18;
+  uint256 constant HAI_PRICE = 1e18;
+
+  function rad(uint256 wad) internal pure returns (uint256) {
+    return wad * ONE;
+  }
+
+  function setUp() public {
+    hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+
+    ISAFEEngine.SAFEEngineParams memory _safeEngineParams =
+      ISAFEEngine.SAFEEngineParams({safeDebtCeiling: type(uint256).max, globalDebtCeiling: 0});
+
+    safeEngine = new SAFEEngine(_safeEngineParams);
+
+    protocolToken = new CoinForTest('', '');
+    protocolTokenJoin = new CollateralJoin(address(safeEngine), KITE, address(protocolToken));
+    safeEngine.addAuthorization(address(protocolTokenJoin));
+
+    systemCoinOracle = new OracleForTest(HAI_PRICE);
+
+    IOracleRelayer.OracleRelayerParams memory _oracleRelayerParams =
+      IOracleRelayer.OracleRelayerParams({redemptionRateUpperBound: 1e45, redemptionRateLowerBound: 1});
+    oracleRelayer = new OracleRelayerForTest(address(safeEngine), IBaseOracle(systemCoinOracle), _oracleRelayerParams);
+    safeEngine.addAuthorization(address(oracleRelayer));
+    oracleRelayer.setRedemptionPrice(1e27);
+
+    protocolTokenOracle = new OracleForTest(KITE_PRICE);
+
+    oracleRelayer.initializeCollateralType(
+      KITE,
+      abi.encode(
+        IOracleRelayer.OracleRelayerCollateralParams({
+          oracle: IBaseOracle(protocolTokenOracle),
+          safetyCRatio: 1e27,
+          liquidationCRatio: 1e27
+        })
+      )
+    );
+
+    oracleRelayer.updateCollateralPrice(KITE);
+
+    ICollateralAuctionHouse.CollateralAuctionHouseParams memory _pssahParams = ICollateralAuctionHouse
+      .CollateralAuctionHouseParams({
+      minimumBid: 100e18,
+      minDiscount: 1e18,
+      maxDiscount: 0.5e18,
+      perSecondDiscountUpdateRate: MINUS_50_PERCENT_IN_10_HOURS
+    });
+
+    ExpensesAuctioneer.ExpensesAuctioneerParams memory _expensesAuctioneerParams = ExpensesAuctioneer
+      .ExpensesAuctioneerParams({
+      recipient: RECIPIENT,
+      usdAmountPerAuction: 1000 ether, // 1000 USD per auction
+      expensesPerYear: 365 * 1000 ether, // 1000 USD per day
+      sellBuffer: 1.1 ether, // + 10%
+      cooldownPeriod: 1 days,
+      auctionDuration: 1 days
+    });
+
+    expensesAuctioneer = new ExpensesAuctioneer(
+      address(safeEngine), address(protocolTokenJoin), address(oracleRelayer), _expensesAuctioneerParams
+    );
+
+    kiteCollateralAuctionHouse = new CollateralAuctionHouse(
+      address(safeEngine), address(expensesAuctioneer), address(oracleRelayer), KITE, _pssahParams
+    );
+
+    expensesAuctioneer.modifyParameters('collateralAuctionHouse', abi.encode(address(kiteCollateralAuctionHouse)));
+
+    protocolToken.mint(1000 ether);
+    protocolToken.transfer(address(expensesAuctioneer), 1000 ether);
+    expensesAuctioneer.joinKiteBalance();
+
+    safeEngine.createUnbackedDebt(BIDDER, BIDDER, 1000e45);
+    hevm.prank(BIDDER);
+    safeEngine.approveSAFEModification(address(kiteCollateralAuctionHouse));
+  }
+
+  function test_modify_parameters() public {
+    CollateralJoin _newProtocolJoin = new CollateralJoin(address(safeEngine), KITE, address(protocolToken));
+    expensesAuctioneer.modifyParameters('protocolJoin', abi.encode(_newProtocolJoin));
+    hevm.etch(address(0x5678), abi.encode(0xF)); // ensure oracleRelayer has code
+    expensesAuctioneer.modifyParameters('oracleRelayer', abi.encode(0x5678));
+    CollateralAuctionHouse _newCollateralAuctionHouse = new CollateralAuctionHouse(
+      address(safeEngine),
+      address(expensesAuctioneer),
+      address(oracleRelayer),
+      KITE,
+      ICollateralAuctionHouse.CollateralAuctionHouseParams({
+        minimumBid: 100e18,
+        minDiscount: 1e18,
+        maxDiscount: 0.5e18,
+        perSecondDiscountUpdateRate: MINUS_50_PERCENT_IN_10_HOURS
+      })
+    );
+    expensesAuctioneer.modifyParameters('collateralAuctionHouse', abi.encode(_newCollateralAuctionHouse));
+    expensesAuctioneer.modifyParameters('recipient', abi.encode(0x13141516));
+    expensesAuctioneer.modifyParameters('usdAmountPerAuction', abi.encode(100));
+    expensesAuctioneer.modifyParameters('expensesPerYear', abi.encode(200));
+    expensesAuctioneer.modifyParameters('sellBuffer', abi.encode(300));
+    expensesAuctioneer.modifyParameters('cooldownPeriod', abi.encode(400));
+    expensesAuctioneer.modifyParameters('auctionDuration', abi.encode(500));
+
+    assertTrue(address(expensesAuctioneer.protocolJoin()) == address(_newProtocolJoin));
+    assertTrue(address(expensesAuctioneer.oracleRelayer()) == address(0x5678));
+    assertTrue(address(expensesAuctioneer.collateralAuctionHouse()) == address(_newCollateralAuctionHouse));
+    assertTrue(expensesAuctioneer.params().recipient == address(0x13141516));
+    assertTrue(expensesAuctioneer.params().usdAmountPerAuction == 100);
+    assertTrue(expensesAuctioneer.params().expensesPerYear == 200);
+    assertTrue(expensesAuctioneer.params().sellBuffer == 300);
+    assertTrue(expensesAuctioneer.params().cooldownPeriod == 400);
+    assertTrue(expensesAuctioneer.params().auctionDuration == 500);
+  }
+
+  function test_auction_expenses() public {
+    hevm.warp(block.timestamp + 1 days);
+    uint256 _auctionId = expensesAuctioneer.auctionExpenses();
+
+    ICollateralAuctionHouse.Auction memory _auction = kiteCollateralAuctionHouse.auctions(_auctionId);
+
+    assertEq(_auction.amountToSell, 110 ether); // sell 100 KITE ($1000) + 10% buffer
+    assertEq(_auction.amountToRaise, 1000e45); // raise 1000 HAI (@1 RP)
+    assertEq(_auction.initialTimestamp, block.timestamp);
+    assertEq(_auction.forgoneCollateralReceiver, address(expensesAuctioneer));
+    assertEq(_auction.auctionIncomeRecipient, RECIPIENT);
+  }
+
+  function test_buy_auction() public {
+    hevm.warp(block.timestamp + 1 days);
+    uint256 _auctionId = expensesAuctioneer.auctionExpenses();
+    uint256 _outstandingDebt = expensesAuctioneer.outstandingDebt();
+
+    hevm.prank(BIDDER);
+    kiteCollateralAuctionHouse.buyCollateral(_auctionId, 1000e18);
+
+    // Check that the auction is deleted
+    ICollateralAuctionHouse.Auction memory _auction = kiteCollateralAuctionHouse.auctions(_auctionId);
+    assertEq(_auction.amountToSell, 0);
+    assertEq(_auction.amountToRaise, 0);
+    assertEq(_auction.initialTimestamp, 0);
+    assertEq(_auction.forgoneCollateralReceiver, address(0));
+    assertEq(_auction.auctionIncomeRecipient, address(0));
+
+    // Check that it covered 1000 HAI (@1 RP) = $1000 of debt
+    assertEq(_outstandingDebt - expensesAuctioneer.outstandingDebt(), 1000e18);
+  }
+
+  function test_partial_buy_auction() public {
+    hevm.warp(block.timestamp + 1 days);
+    uint256 _auctionId = expensesAuctioneer.auctionExpenses();
+    uint256 _outstandingDebt = expensesAuctioneer.outstandingDebt();
+
+    hevm.prank(BIDDER);
+    kiteCollateralAuctionHouse.buyCollateral(_auctionId, 500e18);
+
+    // Check that the auction is deleted
+    ICollateralAuctionHouse.Auction memory _auction = kiteCollateralAuctionHouse.auctions(_auctionId);
+    assertEq(_auction.amountToSell, 60 ether); // 50 KITE ($500) + 10% buffer
+    assertEq(_auction.amountToRaise, 500e45); // raise 500 HAI (@1 RP)
+    assertEq(_auction.initialTimestamp, block.timestamp);
+    assertEq(_auction.forgoneCollateralReceiver, address(expensesAuctioneer));
+    assertEq(_auction.auctionIncomeRecipient, address(RECIPIENT));
+
+    // Check that it covered 1000 HAI (@1 RP) = $1000 of debt
+    assertEq(_outstandingDebt - expensesAuctioneer.outstandingDebt(), 500e18);
+  }
+
+  function test_buy_auction_with_new_redemption_price() public {
+    hevm.warp(block.timestamp + 1 days);
+    uint256 _auctionId = expensesAuctioneer.auctionExpenses();
+    uint256 _outstandingDebt = expensesAuctioneer.outstandingDebt();
+
+    oracleRelayer.setRedemptionPrice(2e27);
+
+    hevm.prank(BIDDER);
+    kiteCollateralAuctionHouse.buyCollateral(_auctionId, 500e18);
+
+    // Check that the auction is deleted
+    ICollateralAuctionHouse.Auction memory _auction = kiteCollateralAuctionHouse.auctions(_auctionId);
+    assertEq(_auction.amountToSell, 10 ether); // the 10% buffer
+    assertEq(_auction.amountToRaise, 500e45); // raise 500 HAI (@2 RP)
+
+    // Check that it covered 500 HAI (@2 RP) = $1000 of debt
+    assertEq(_outstandingDebt - expensesAuctioneer.outstandingDebt(), 1000e18);
+  }
+
+  function test_buy_auction_with_new_kite_price() public {
+    hevm.warp(block.timestamp + 1 days);
+    uint256 _auctionId = expensesAuctioneer.auctionExpenses();
+    uint256 _outstandingDebt = expensesAuctioneer.outstandingDebt();
+
+    protocolTokenOracle.setPriceAndValidity(5e18, true);
+
+    hevm.prank(BIDDER);
+    kiteCollateralAuctionHouse.buyCollateral(_auctionId, 500e18);
+
+    // Check that the auction is deleted
+    ICollateralAuctionHouse.Auction memory _auction = kiteCollateralAuctionHouse.auctions(_auctionId);
+    assertEq(_auction.amountToSell, 10 ether); // the 10% buffer
+    assertEq(_auction.amountToRaise, 500e45); // raise 500 HAI (@1 RP)
+
+    // Check that it covered 500 HAI (@1 RP) = $500 of debt
+    assertEq(_outstandingDebt - expensesAuctioneer.outstandingDebt(), 500e18);
+  }
+
+  function test_buy_auction_with_discounted_kite_price() public {
+    hevm.warp(block.timestamp + 1 days);
+    uint256 _auctionId = expensesAuctioneer.auctionExpenses();
+
+    hevm.warp(block.timestamp + 10 * 3600);
+    expensesAuctioneer.pokeExpenses();
+    uint256 _outstandingDebt = expensesAuctioneer.outstandingDebt();
+
+    hevm.prank(BIDDER);
+    kiteCollateralAuctionHouse.buyCollateral(_auctionId, 500e18);
+
+    // Check that the auction is deleted
+    ICollateralAuctionHouse.Auction memory _auction = kiteCollateralAuctionHouse.auctions(_auctionId);
+    assertEq(_auction.amountToSell, 10 ether); // the 10% buffer
+    assertEq(_auction.amountToRaise, 500e45); // raise 500 HAI (@1 RP)
+
+    // Check that it covered 500 HAI (@1 RP) = $500 of debt
+    assertEq(_outstandingDebt - expensesAuctioneer.outstandingDebt(), 500e18);
+  }
+
+  function test_cooldown() public {
+    hevm.warp(block.timestamp + 1 days);
+    expensesAuctioneer.auctionExpenses();
+
+    hevm.expectRevert(ExpensesAuctioneer.AuctioneerIsInCooldown.selector);
+    expensesAuctioneer.auctionExpenses();
+
+    hevm.warp(block.timestamp + 1 days);
+    expensesAuctioneer.auctionExpenses();
+  }
+
+  function test_terminate_auction() public {
+    hevm.warp(block.timestamp + 1 days);
+    uint256 _auctionId = expensesAuctioneer.auctionExpenses();
+
+    hevm.expectRevert(ExpensesAuctioneer.AuctionIsStillActive.selector);
+    expensesAuctioneer.terminateAuction(_auctionId);
+
+    hevm.warp(block.timestamp + 1 days);
+    expensesAuctioneer.terminateAuction(_auctionId);
+  }
+
+  function test_not_enough_debt() public {
+    hevm.expectRevert(ExpensesAuctioneer.NotEnoughDebt.selector);
+    expensesAuctioneer.auctionExpenses();
+  }
+}
