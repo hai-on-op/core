f970102ca3d2d929d9e9172001534f4e7c27c650
Luke Hamilton <pies.eth@pm.me>
2025-03-31 21:52:39 -0400

    fix: W4: Unchecked return value of ERC20.transfer in RewardDistributor

--- src/contracts/tokens/RewardDistributor.sol
+++ src/contracts/tokens/RewardDistributor.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {SafeERC20} from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import {MerkleProof} from '@openzeppelin/contracts/utils/cryptography/MerkleProof.sol';
 import {Pausable} from '@openzeppelin/contracts/utils/Pausable.sol';
@@ -18,6 +19,7 @@ import {Encoding} from '@libraries/Encoding.sol';
 
 contract RewardDistributor is Authorizable, Modifiable, Pausable, IRewardDistributor {
   using Encoding for bytes;
+  using SafeERC20 for IERC20;
 
   // --- Data ---
 
