a6481928f98a91e96bf4d8572f194a639173e6af
Luke Hamilton <pies.eth@pm.me>
2025-04-04 13:36:59 -0400

    fix: linting

--- src/contracts/tokens/RewardDistributor.sol
+++ src/contracts/tokens/RewardDistributor.sol
@@ -1,182 +1,142 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import {MerkleProof} from '@openzeppelin/contracts/utils/cryptography/MerkleProof.sol';
+import {Pausable} from '@openzeppelin/contracts/utils/Pausable.sol';
 
-import {IRewardDistributor} from "@interfaces/tokens/IRewardDistributor.sol";
+import {IRewardDistributor} from '@interfaces/tokens/IRewardDistributor.sol';
 
-import {Authorizable} from "@contracts/utils/Authorizable.sol";
-import {Modifiable} from "@contracts/utils/Modifiable.sol";
+import {Authorizable} from '@contracts/utils/Authorizable.sol';
+import {Modifiable} from '@contracts/utils/Modifiable.sol';
 
-import {Assertions} from "@libraries/Assertions.sol";
-import {Encoding} from "@libraries/Encoding.sol";
+import {Assertions} from '@libraries/Assertions.sol';
+import {Encoding} from '@libraries/Encoding.sol';
 /**
  * @title  RewardDistributor
  * @notice This contract distributes rewards to users to claim every 24 hours
  */
 
-contract RewardDistributor is
-    Authorizable,
-    Modifiable,
-    Pausable,
-    IRewardDistributor
-{
-    using Encoding for bytes;
-    using Assertions for uint256;
-    // --- Data ---
+contract RewardDistributor is Authorizable, Modifiable, Pausable, IRewardDistributor {
+  using Encoding for bytes;
+  using Assertions for uint256;
+  // --- Data ---
 
-    /// @inheritdoc IRewardDistributor
-    uint256 public epochCounter;
-    /// @inheritdoc IRewardDistributor
-    uint256 public epochDuration;
-    /// @inheritdoc IRewardDistributor
-    uint256 public lastUpdatedTime;
-    /// @inheritdoc IRewardDistributor
-    address public rootSetter;
+  /// @inheritdoc IRewardDistributor
+  uint256 public epochCounter;
+  /// @inheritdoc IRewardDistributor
+  uint256 public epochDuration;
+  /// @inheritdoc IRewardDistributor
+  uint256 public lastUpdatedTime;
+  /// @inheritdoc IRewardDistributor
+  address public rootSetter;
 
-    /// @inheritdoc IRewardDistributor
-    mapping(address _token => bytes32 _root) public merkleRoots;
-    /// @inheritdoc IRewardDistributor
-    mapping(bytes32 _root => mapping(address _account => bool _hasClaimed))
-        public isClaimed;
+  /// @inheritdoc IRewardDistributor
+  mapping(address _token => bytes32 _root) public merkleRoots;
+  /// @inheritdoc IRewardDistributor
+  mapping(bytes32 _root => mapping(address _account => bool _hasClaimed)) public isClaimed;
 
-    // --- Init ---
-    constructor(
-        uint256 _epochDuration,
-        address _rootSetter
-    ) Authorizable(msg.sender) {
-        epochDuration = _epochDuration;
-        epochCounter = 0;
-        rootSetter = _rootSetter;
+  // --- Init ---
+  constructor(uint256 _epochDuration, address _rootSetter) Authorizable(msg.sender) {
+    epochDuration = _epochDuration;
+    epochCounter = 0;
+    rootSetter = _rootSetter;
+  }
+
+  // --- Methods ---
+
+  /// @inheritdoc IRewardDistributor
+  function pause() external isAuthorized {
+    _pause();
+  }
+
+  /// @inheritdoc IRewardDistributor
+  function unpause() external isAuthorized {
+    _unpause();
+  }
+
+  /// @inheritdoc IRewardDistributor
+  function updateMerkleRoots(address[] calldata _tokens, bytes32[] calldata _merkleRoots) external {
+    if (block.timestamp - lastUpdatedTime < epochDuration) {
+      revert RewardDistributor_TooSoonEpochNotElapsed();
+    }
+    if (msg.sender != rootSetter) revert RewardDistributor_NotRootSetter();
+    if (_tokens.length != _merkleRoots.length) {
+      revert RewardDistributor_ArrayLengthsMustMatch();
     }
 
-    // --- Methods ---
+    for (uint256 _i = 0; _i < _tokens.length; _i++) {
+      if (_tokens[_i] == address(0)) {
+        revert RewardDistributor_InvalidTokenAddress();
+      }
+      if (_merkleRoots[_i] == bytes32(0)) {
+        revert RewardDistributor_InvalidMerkleRoot();
+      }
+      merkleRoots[_tokens[_i]] = _merkleRoots[_i];
+      emit RewardDistributorMerkleRootUpdated(_tokens[_i], _merkleRoots[_i], epochCounter);
+    }
+    lastUpdatedTime = block.timestamp;
+    epochCounter++;
+  }
 
-    /// @inheritdoc IRewardDistributor
-    function pause() external isAuthorized {
-        _pause();
+  /// @inheritdoc IRewardDistributor
+  function claim(address _token, uint256 _wad, bytes32[] calldata _merkleProof) external whenNotPaused {
+    _claim(_token, _wad, _merkleProof);
+  }
+
+  /// @inheritdoc IRewardDistributor
+  function multiClaim(
+    address[] calldata _tokens,
+    uint256[] calldata _wads,
+    bytes32[][] calldata _merkleProofs
+  ) external whenNotPaused {
+    if (_tokens.length != _wads.length || _wads.length != _merkleProofs.length) {
+      revert RewardDistributor_ArrayLengthsMustMatch();
     }
 
-    /// @inheritdoc IRewardDistributor
-    function unpause() external isAuthorized {
-        _unpause();
+    for (uint256 _i = 0; _i < _tokens.length; _i++) {
+      _claim(_tokens[_i], _wads[_i], _merkleProofs[_i]);
+    }
+  }
+
+  /// @inheritdoc IRewardDistributor
+  function emergencyWithdraw(address _rescueReceiver, address _token, uint256 _wad) external isAuthorized {
+    if (_token == address(0)) {
+      revert RewardDistributor_InvalidTokenAddress();
+    }
+    if (_wad == 0) revert RewardDistributor_InvalidAmount();
+    IERC20(_token).transfer(_rescueReceiver, _wad);
+    emit RewardDistributorEmergencyWithdrawal(_rescueReceiver, _token, _wad);
+  }
+
+  function _claim(address _token, uint256 _wad, bytes32[] calldata _merkleProof) internal {
+    if (isClaimed[merkleRoots[_token]][msg.sender]) {
+      revert RewardDistributor_AlreadyClaimed();
     }
 
-    /// @inheritdoc IRewardDistributor
-    function updateMerkleRoots(
-        address[] calldata _tokens,
-        bytes32[] calldata _merkleRoots
-    ) external {
-        if (block.timestamp - lastUpdatedTime < epochDuration) {
-            revert RewardDistributor_TooSoonEpochNotElapsed();
-        }
-        if (msg.sender != rootSetter) revert RewardDistributor_NotRootSetter();
-        if (_tokens.length != _merkleRoots.length) {
-            revert RewardDistributor_ArrayLengthsMustMatch();
-        }
+    bytes32 _leaf = keccak256(bytes.concat(keccak256(abi.encode(address(msg.sender), _wad))));
 
-        for (uint256 _i = 0; _i < _tokens.length; _i++) {
-            if (_tokens[_i] == address(0)) {
-                revert RewardDistributor_InvalidTokenAddress();
-            }
-            if (_merkleRoots[_i] == bytes32(0)) {
-                revert RewardDistributor_InvalidMerkleRoot();
-            }
-            merkleRoots[_tokens[_i]] = _merkleRoots[_i];
-            emit RewardDistributorMerkleRootUpdated(
-                _tokens[_i],
-                _merkleRoots[_i],
-                epochCounter
-            );
-        }
-        lastUpdatedTime = block.timestamp;
-        epochCounter++;
+    if (MerkleProof.verify(_merkleProof, merkleRoots[_token], _leaf)) {
+      bool _success = IERC20(_token).transfer(msg.sender, _wad);
+      if (!_success) revert RewardDistributor_TransferFailed();
+      isClaimed[merkleRoots[_token]][msg.sender] = true;
+      emit RewardDistributorRewardClaimed(msg.sender, _token, _wad);
+    } else {
+      revert RewardDistributor_InvalidMerkleProof();
     }
+  }
 
-    /// @inheritdoc IRewardDistributor
-    function claim(
-        address _token,
-        uint256 _wad,
-        bytes32[] calldata _merkleProof
-    ) external whenNotPaused {
-        _claim(_token, _wad, _merkleProof);
-    }
+  // --- Administration ---
 
-    /// @inheritdoc IRewardDistributor
-    function multiClaim(
-        address[] calldata _tokens,
-        uint256[] calldata _wads,
-        bytes32[][] calldata _merkleProofs
-    ) external whenNotPaused {
-        if (
-            _tokens.length != _wads.length ||
-            _wads.length != _merkleProofs.length
-        ) {
-            revert RewardDistributor_ArrayLengthsMustMatch();
-        }
+  /// @inheritdoc Modifiable
+  function _modifyParameters(bytes32 _param, bytes memory _data) internal override {
+    if (_param == 'epochDuration') epochDuration = _data.toUint256();
+    else if (_param == 'rootSetter') rootSetter = _data.toAddress();
+    else revert UnrecognizedParam();
+  }
 
-        for (uint256 _i = 0; _i < _tokens.length; _i++) {
-            _claim(_tokens[_i], _wads[_i], _merkleProofs[_i]);
-        }
-    }
-
-    /// @inheritdoc IRewardDistributor
-    function emergencyWithdraw(
-        address _rescueReceiver,
-        address _token,
-        uint256 _wad
-    ) external isAuthorized {
-        if (_token == address(0)) {
-            revert RewardDistributor_InvalidTokenAddress();
-        }
-        if (_wad == 0) revert RewardDistributor_InvalidAmount();
-        IERC20(_token).transfer(_rescueReceiver, _wad);
-        emit RewardDistributorEmergencyWithdrawal(
-            _rescueReceiver,
-            _token,
-            _wad
-        );
-    }
-
-    function _claim(
-        address _token,
-        uint256 _wad,
-        bytes32[] calldata _merkleProof
-    ) internal {
-        if (isClaimed[merkleRoots[_token]][msg.sender]) {
-            revert RewardDistributor_AlreadyClaimed();
-        }
-
-        bytes32 _leaf = keccak256(
-            bytes.concat(keccak256(abi.encode(address(msg.sender), _wad)))
-        );
-
-        if (MerkleProof.verify(_merkleProof, merkleRoots[_token], _leaf)) {
-            bool success = IERC20(_token).transfer(msg.sender, _wad);
-            if (!success) revert RewardDistributor_TransferFailed();
-            isClaimed[merkleRoots[_token]][msg.sender] = true;
-            emit RewardDistributorRewardClaimed(msg.sender, _token, _wad);
-        } else {
-            revert RewardDistributor_InvalidMerkleProof();
-        }
-    }
-
-    // --- Administration ---
-
-    /// @inheritdoc Modifiable
-    function _modifyParameters(
-        bytes32 _param,
-        bytes memory _data
-    ) internal override {
-        if (_param == "epochDuration") epochDuration = _data.toUint256();
-        else if (_param == "rootSetter") rootSetter = _data.toAddress();
-        else revert UnrecognizedParam();
-    }
-
-    /// @inheritdoc Modifiable
-    function _validateParameters() internal view override {
-        epochDuration.assertGt(0);
-    }
+  /// @inheritdoc Modifiable
+  function _validateParameters() internal view override {
+    epochDuration.assertGt(0);
+  }
 }
