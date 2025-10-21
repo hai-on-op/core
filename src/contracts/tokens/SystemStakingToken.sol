57ef80de244f8314b196ff0c47afd43117784fa3
Luke Hamilton <pies.eth@pm.me>
2025-10-21 13:39:58 -0400

    feat: system staking token (sHAI)

--- src/contracts/tokens/SystemStakingToken.sol
+++ src/contracts/tokens/SystemStakingToken.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.20;
 
+import {ISystemCoin} from '@interfaces/tokens/ISystemCoin.sol';
 import {ISystemStakingToken} from '@interfaces/tokens/ISystemStakingToken.sol';
+
+import {IStabilityPool} from '@interfaces/IStabilityPool.sol';
 
 import {ERC20} from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import {ERC20Permit, IERC20Permit} from '@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol';
@@ -14,4 +17,99 @@ import {Modifiable} from '@contracts/utils/Modifiable.sol';
  * @title  SystemStakingToken
  * @notice This contract represents the staked system coin
  */
-contract SystemStakingToken is ERC20, ERC20Permit, ERC20Burnable, Authorizable, Modifiable, ISystemStakingToken {}
+contract SystemStakingToken is ERC20, ERC20Permit, ERC20Burnable, Authorizable, Modifiable, ISystemStakingToken {
+  // --- Registry ---
+  /// @inheritdoc ISystemStakingToken
+  ISystemCoin public systemCoin;
+
+  /// @inheritdoc ISystemStakingToken
+  IStabilityPool public stabilityPool;
+
+  // --- Init ---
+
+  /**
+   * @param  _name String with the name of the token
+   * @param  _symbol String with the symbol of the token
+   * @param  _systemCoin Address of the system coin
+   */
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    address _systemCoin
+  ) ERC20(_name, _symbol) ERC20Permit(_name) Authorizable(msg.sender) {
+    if (_systemCoin == address(0)) {
+      revert SystemStakingToken_NullSystemCoin();
+    }
+    systemCoin = ISystemCoin(_systemCoin);
+  }
+
+  // --- Methods ---
+
+  /// @inheritdoc ISystemStakingToken
+  function mint(address _dst, uint256 _wad) external isAuthorized {
+    _mint(_dst, _wad);
+    emit SystemStakingTokenMint(_dst, _wad);
+  }
+
+  /// @inheritdoc ISystemStakingToken
+  function burn(uint256 _wad) external {
+    _burn(msg.sender, _wad);
+    emit SystemStakingTokenBurn(msg.sender, _wad);
+  }
+
+  /// @inheritdoc ISystemStakingToken
+  function burnFrom(address _account, uint256 _wad) external {
+    _spendAllowance(_account, msg.sender, _wad);
+    _burn(_account, _wad);
+    emit SystemStakingTokenBurn(_account, _wad);
+  }
+
+  // --- Overrides ---
+
+  function _update(address _from, address _to, uint256 _value) internal override(ERC20, ERC20Votes) {
+    if (address(stabilityPool) == address(0)) {
+      revert SystemStakingToken_NullStabilityPool();
+    }
+
+    if (_from != address(0) && _to != address(0)) {
+      // Normal transger, settle rewards and move staked weight
+      stabilityPool.onTokenTransfer(_from, _to, _value);
+    } else {
+      // Mint or burn, only checkpoint
+      stabilityPool.checkpoint([_from, _to]);
+    }
+    super._update(_from, _to, _value);
+  }
+
+  function nonces(address _owner) public view override(ERC20Permit, IERC20Permit, Nonces) returns (uint256 _nonce) {
+    return super.nonces(_owner);
+  }
+
+  /**
+   * Set the clock to block timestamp, as opposed to the default block number.
+   */
+  function clock() public view override returns (uint48 _timestamp) {
+    return Time.timestamp();
+  }
+
+  // solhint-disable-next-line func-name-mixedcase
+  function CLOCK_MODE() public view virtual override returns (string memory _mode) {
+    return 'mode=timestamp';
+  }
+
+  // --- Administration ---
+
+  /// @inheritdoc Modifiable
+  function _modifyParameters(bytes32 _param, bytes memory _data) internal override {
+    if (_param == 'stabilityPool') {
+      stabilityPool = IStabilityPool(_data.toAddress());
+    } else {
+      revert UnrecognizedParam();
+    }
+  }
+
+  /// @inheritdoc Modifiable
+  function _validateParameters() internal view override {
+    address(stabilityPool).assertHasCode();
+  }
+}
--- src/interfaces/tokens/ISystemStakingToken.sol
+++ src/interfaces/tokens/ISystemStakingToken.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.20;
 
+import {ISystemCoin} from '@interfaces/tokens/ISystemCoin.sol';
+
+import {IStabilityPool} from '@interfaces/IStabilityPool.sol';
+
 import {IERC20Metadata} from '@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol';
 import {IERC20Permit} from '@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol';
 
@@ -10,4 +14,66 @@ import {IAuthorizable} from '@interfaces/utils/IAuthorizable.sol';
  * @title  ISystemStakingToken
  * @notice Interface for the SystemStakingToken contract which represents staked system coins
  */
-interface ISystemStakingToken is IERC20Metadata, IERC20Permit, IAuthorizable {}
+interface ISystemStakingToken is IERC20Metadata, IERC20Permit, IAuthorizable {
+  // --- Events ---
+
+  /**
+   * @notice Emitted when tokens are minted
+   * @param _dst The address that received the minted tokens
+   * @param _wad The amount of tokens minted [wad]
+   */
+  event SystemStakingTokenMint(address indexed _dst, uint256 _wad);
+
+  /**
+   * @notice Emitted when tokens are burned
+   * @param _src The address whose tokens were burned
+   * @param _wad The amount of tokens burned [wad]
+   */
+  event SystemStakingTokenBurn(address indexed _src, uint256 _wad);
+
+  // --- Errors ---
+
+  /// @notice Throws when StabilityPool is null
+  error SystemStakingToken_NullStabilityPool();
+
+  /// @notice Throws when SystemCoin is null
+  error SystemStakingToken_NullSystemCoin();
+
+  // --- Registry ---
+
+  /**
+   * @notice Address of the system coin
+   * @return _systemCoin The system coin contract
+   */
+  function systemCoin() external view returns (ISystemCoin _systemCoin);
+
+  /**
+   * @notice Address of the stability pool
+   * @return _stabilityPool The stability pool contract
+   */
+  function stabilityPool() external view returns (IStabilityPool _stabilityPool);
+
+  // --- Methods ---
+
+  /**
+   * @notice Mints new tokens to the specified address
+   * @dev Only callable by authorized addresses
+   * @param _dst The address to mint tokens to
+   * @param _wad The amount of tokens to mint
+   */
+  function mint(address _dst, uint256 _wad) external;
+
+  /**
+   * @notice Burns tokens from the caller's address
+   * @param _wad The amount of tokens to burn
+   */
+  function burn(uint256 _wad) external;
+
+  /**
+   * /**
+   * @notice Burns tokens from the caller's address
+   * @param _account Address of the account to mint tokens to
+   * @param _wad The amount of tokens to burn
+   */
+  function burnFrom(address _account, uint256 _wad) external;
+}
